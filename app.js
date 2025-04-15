8eee477a37671bf6741040de166652eb3b7e3ef8
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2025-04-14 21:19:21 -0300

    tradução e adição de projetos

--- app.js
+++ app.js
@@ -3,47 +3,16 @@ const path = require('path');
 
 const app = express();
 
-// Configuração do EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// Middleware para arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Dados globais
-const globalData = {
-    email: "seu.email@exemplo.com",
-    phone: "(00) 00000-0000",
-    address: "Sua Cidade, Estado",
-    github: "https://github.com/seu-usuario",
-    linkedin: "https://linkedin.com/in/seu-usuario",
-    instagram: "https://instagram.com/seu-usuario"
-};
+const globalData = require('./public/global_data.json');
+const projects = require('./public/projects.json');
 
-// Dados dos projetos
-const projects = [
-    {
-        id: 1,
-        name: "Projeto 1",
-        description: "Descrição detalhada do projeto 1",
-        image: "https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg",
-        github: "https://github.com/seu-usuario/projeto1"
-    },
-    {
-        id: 2,
-        name: "Projeto 2",
-        description: "Descrição detalhada do projeto 2",
-        image: "https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg",
-        github: "https://github.com/seu-usuario/projeto2"
-    }
-];
-
-const meu_nome = "pedro garcia"
-
-// Rotas
 app.get('/', (req, res) => {
     res.render('index', { 
-        meu_nome: meu_nome
+        global_data: globalData,
     });
 });
 
@@ -54,7 +23,7 @@ app.get('/projetos', (req, res) => {
     });
 });
 
-// Iniciar o servidor
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
