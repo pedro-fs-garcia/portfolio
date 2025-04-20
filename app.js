1ec0577cb45b1a27102aafa196d423ff7032becd
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2025-04-19 22:50:08 -0300

    correções

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 const globalData = require('./public/global_data.json');
 const projects = require('./public/projects.json');
